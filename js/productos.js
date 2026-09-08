163a3ef17be96d9b8695b8254fb643b7b11c6ff2
LostForsaked <ruarteag@gmail.com>
2026-09-08 11:04:07 -0300

    productos agregados

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,14 @@
 const productosCatalogo = [
-  { id: 1, titulo: "Product Title 1", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 1" },
-  { id: 2, titulo: "Polera Cat Meme", precio: 11000, img: "img/catmeme.png", descripcion: "Polera negra de algodón con estampado de memes de gatos." },
-  { id: 3, titulo: "Product Title 3", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 3" },
-  { id: 4, titulo: "Product Title 4", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 4" },
-  { id: 5, titulo: "Product Title 5", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 5" },
-  { id: 6, titulo: "Product Title 6", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 6" },
-  { id: 7, titulo: "Product Title 7", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 7" },
-  { id: 8, titulo: "Product Title 8", precio: 1000, img: "img/Logo.jpg", descripcion: "Descripción del producto 8" }
+  { id: 1, titulo: "Afraid of Women", precio: 14990, img: "img/poleraLobo.avif", descripcion: "Polera negra con estampado de lobo" },
+  { id: 2, titulo: "Polera Cat Meme", precio: 14990, img: "img/catmeme.png", descripcion: "Polera negra de algodón con estampado de memes de gatos." },
+  { id: 3, titulo: "Gato SixSeven", precio: 14990, img: "img/sixseven.webp", descripcion: "Meme - Gato Sixseven 2 - Polera" },
+  { id: 4, titulo: "Autism", precio: 14990, img: "img/autism.webp", descripcion: "Autism esqueleto AK - Esqueleto - Polera" },
+  { id: 5, titulo: "The cat parade", precio: 14990, img: "img/catparade.webp", descripcion: "My Chemical Romance - The Cat Parade - Polera" },
+  { id: 6, titulo: "System of a down", precio: 14990, img: "img/soad.webp", descripcion: "System Of a Down - Vision Logo - Polera" },
+  { id: 7, titulo: "Ride the lightning", precio: 14990, img: "img/metallica.webp", descripcion: "Metallica - Ride The Lightning - Polera" },
+  { id: 8, titulo: "Bring me the horizon", precio: 14990, img: "img/bmth.webp", descripcion: "Bring Me The Horizon - Rock - Polera" },
+  { id: 9, titulo: "Asking Alexandria", precio: 14990, img: "img/aa.webp", descripcion: "Asking Alexandria - Dead - Polera" },
+  { id: 10, titulo: "Bullet for my valentine", precio: 14990, img: "img/bfmv.webp", descripcion: "Bullet For My Valentine - Skull Emo 2 - Polera" },
+  { id: 11, titulo: "The devil wears prada", precio: 14990, img: "img/tdwp.webp", descripcion: "The Devil Wears Prada - Gargoyle - Polera" },
+  { id: 12, titulo: "Vader samurai", precio: 14990, img: "img/vader.webp", descripcion: "Star Wars - Darth Vader Samurai - Polera" }
 ];
